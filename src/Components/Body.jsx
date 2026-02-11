8c06129113b6411eb46b968607933b0d70b94659
Shayan Sheikh <shayan.faisal1095@gmail.com>
2026-02-10 20:21:43 -0500

    done

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,7 +1,36 @@
 
-
+import { useState } from "react";
 
 const Body = () => {
+
+
+    const [properties, setProperties] = useState({
+
+        toptext : "One does not simply",
+        bottomtext: "Walk into Mordor",
+        imgUrl : "http://i.imgflip.com/1bij.jpg"
+
+
+
+
+
+    })
+
+
+    function handleChange(event){
+
+
+        
+
+      const { name, value } = event.target
+        setProperties(prev => ({
+            ...prev,
+             [name]: value
+        }))
+
+
+
+    }
 
 return( 
 
@@ -11,17 +40,26 @@ return(
 
   <div className="Input">
     <label >Top Text
-    <input type="text" name= "topText" placeholder="Enter text here"/>
+    <input type="text" name= "toptext" placeholder="Enter text here" onChange={handleChange} value={properties.toptext}/>
 </label>
     <label >Bottom Text
-    <input type="text" name= "bottomText" placeholder="Enter text here"/>
+    <input type="text" name= "bottomtext" placeholder="Enter text here" value = {properties.bottomtext} onChange={handleChange}/>
     </label>
 
 
     <button>Get a new meme image 🖼</button>
 
+
+
+
 </div>
 
+
+     <div className="meme">
+                <img src={properties.imgUrl} />
+                <span className="top">{properties.toptext}</span>
+                <span className="bottom">{properties.bottomtext}</span>
+            </div>
 
 
 
